7beadb5e40c2a42b2c0665f8a0cc5ea9fe6231eb
Mech-Kal <kalpakkamble02@gmail.com>
2025-11-05 02:56:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the element with id "level"
+const element = document.getElementById('level');
+
+let level = 0;
+let current = element;
+
+// Traverse up the DOM tree until there are no more parents
+while (current) {
+  level++;
+  current = current.parentElement;
+}
+
+// Display the result
+alert(`The level of the element is: ${level}`);
